20074db8e7d35ed908d76e427364ac4bd86f1fba
Rodrigo Reis <roreis99@gmail.com>
2020-02-29 12:21:50 -0300

    feat: finish first project with vue not using components

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const vm = new Vue({
   data: {
     products: [],
     product: false,
+    minicart: [],
+    minicartToggle: false,
+    messageAlert: 'Item adicionado',
+    alertActive: false,
   },
   filters: {
     formatedPrice(value){
@@ -11,6 +15,17 @@ const vm = new Vue({
         currency: 'BRL'
       });
     }
+  },
+  computed: {
+    minicartTotal(){
+      let total = 0;
+      if (this.minicart.length) {
+        this.minicart.forEach(({ preco }) => {
+          total += preco;
+        });
+      }
+      return total;
+    },
   },
   methods: {
     async fetchProducts() {
@@ -27,13 +42,74 @@ const vm = new Vue({
         const response = await fetch(`../src/api/products/${id}/dados.json`);
         const data = await response.json();
         this.product = data;
-        console.log(this.product);
       } catch (error) {
         console.log('bar', error)
+      }
+    },
+    openModal(id){
+      this.fetchProduct(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth" 
+      })
+    },
+    closeModal({ target, currentTarget }){
+      if(target === currentTarget){
+        this.product = false;
+      }
+    },
+    closeCart({ target, currentTarget }){
+      if(target === currentTarget){
+        this.minicartToggle = false;
+      }
+    },
+    equateStock(){
+      const items = this.minicart.filter(({ id }) => id === this.product.id);
+      this.product.estoque -= items.length;
+    },
+    addItem(){
+      this.product.estoque--;
+      const { id, nome, preco } = this.product;
+      this.minicart.push({id, nome, preco});
+      this.alert(`${nome} foi adicionado ao carrinho.`)
+    },
+    removeItem(index){
+      this.minicart.splice(index, 1)
+    },
+    checkLocalStorage(){
+      if(window.localStorage.minicart){
+        this.minicart = JSON.parse(window.localStorage.minicart);
+      }
+    },
+    alert(message){
+      this.messageAlert = message;
+      this.alertActive = true;
+      setTimeout(() => {
+        this.alertActive = false;
+      }, 1200)
+    },
+    router(){
+      const hash = document.location.hash;
+      if(hash){
+        this.fetchProduct(hash.replace('#', ''))
+      }
+    }
+  },
+  watch: {
+    minicart(){
+      window.localStorage.minicart = JSON.stringify(this.minicart);
+    },
+    product(){
+      document.title = `Techno ${this.product.nome || ''}`;
+      history.pushState(null, null, `#${this.product.id || ''}`);
+      if (this.product){
+        this.equateStock();
       }
     }
   },
   created() {
     this.fetchProducts();
+    this.checkLocalStorage();
+    this.router();
   }
 })
